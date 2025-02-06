2fb1f62ed03c0ac6ee424d92fd95ffd2a7eed248
Raymond Lee <raymondllc0408@outlook.com>
2025-02-07 02:57:30 +0800

    type safe component

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,10 +1,21 @@
 import { StyleSheet, Text, View, Image } from "react-native";
-import Colors from "@/src/constants/Colors";
+import Colors from "@constants/Colors";
+import { Product } from "@/src/types";
 
-const ProductListItem = ({ product }) => {
+const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: product.image }} />
+      <Image
+        style={styles.image}
+        source={{ uri: product.image || defaultPizzaImage }}
+      />
 
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
